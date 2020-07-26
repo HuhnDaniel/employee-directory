b357bffc71ee85af4ad428bd030cae19aae7e884
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-07-25 22:15:48 -0500

    add header, remove unnecessary files

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import "./style.css";
+
+function Header() {
+    return (
+        <header>
+            <h1 className="title">Employee Directory</h1>
+        </header>
+    );
+}
+
+export default Header;
