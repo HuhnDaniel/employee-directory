d71d9cf4b9675b2e4b903b35f9a041b2529c9500
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-07-26 12:21:24 -0500

    add footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer>
+            <h3 className="author">Created By: Daniel Moody-Huhn</h3>
+        </footer>
+    );
+}
+
+export default Footer;
